696510c7f7541a9eaabff2051209a9534a340845
Thomas <thomasmckenna12gmail.com>
2021-09-07 19:07:49 -0400

    commit tree cf11f86f17e82dd38f132e3c76fd4c7f1f1b0984

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(name) {
+let employee = {
+    firstName: 'Gray' , 
+    familyName: 'Worm',
+    title: 'Security',
+    payPerHour: 1,
+    timeInEvents: [],
+    timeOutEvents: []
+}
+return employee;
+}
+
+function createEmployeeRecords(array1) {
+    let employeeRecords = [
+        ["moe", "sizlak", "barkeep", 2],
+        ["bartholomew", "simpson", "scamp", 3] 
+    ]
+    return employeeRecords;
+}
